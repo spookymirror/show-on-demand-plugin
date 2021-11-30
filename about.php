cda74d3aa11c2563cde12e89eb6ba4ef1ac9db16
Spooky Mirror LLC <86417642+spookymirror@users.noreply.github.com>
2021-11-30 06:43:46 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
   <div style="margin:0 auto;"> <br />
     <fieldset style="padding: 10px; border: 2px solid #000;">
-      <legend>Template Plugin Info</legend>
+      <legend>Plugin Info</legend>
       <div style="overflow: hidden; padding: 10px;">
     <div>
       <div id='credits'>
